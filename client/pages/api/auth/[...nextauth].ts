986c7433f9cab8fc87e4e73654da4ab28495ade3
dankichi <dan24.yukito@gamil.com>
2024-08-26 18:49:46 +0900

    フォルダの移動

---
+++ client/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (user && bcrypt.compareSync(credentials!.password, user.password)) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    newUser: "/auth/signup",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
